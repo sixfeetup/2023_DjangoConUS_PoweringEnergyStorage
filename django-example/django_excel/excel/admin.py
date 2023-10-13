c25f7056a29377b80d6da502031f8811790c8b09
rehan892 <rehan@sixfeetup.com>
2023-10-14 01:50:44 +0500

    Add sample django project

--- django-example/django_excel/excel/admin.py
+++ django-example/django_excel/excel/admin.py
@@ -1,3 +1,59 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.core import management
+from django.db.models import JSONField
+from django.http import HttpResponseRedirect
+from django.urls import path
 
-# Register your models here.
+from django_json_widget.widgets import JSONEditorWidget
+
+from excel.models import ConstantData, OutputDataTabel
+
+
+@admin.register(ConstantData)
+class ConstantDataAdmin(admin.ModelAdmin):
+    list_display = ["__str__" ,"project_cycles_per_year", "project_term", "constant_throughput", "project_ecap", "project_min_e_capacity", "project_e_losses"]
+    list_filter = ["project_cycles_per_year", "project_term"]
+    actions = ["generate_results"]
+    formfield_overrides = {
+        JSONField: {'widget': JSONEditorWidget},
+    }
+
+    change_list_template = "admin/data_change_list.html"
+
+    def generate_results(modeladmin, request, queryset):
+        OutputDataTabel.generate_output(queryset)
+
+    def ingest_constant_data(self, request):
+        success = management.call_command("add_sheet_data")
+        if success:
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Successfully Ingested Data",
+            )
+        else:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                "Error occurred while ingesting data",
+            )
+        return HttpResponseRedirect("../")
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                "ingest_constant_data/",
+                self.ingest_constant_data,
+                name="ingest_constant_data",
+            ),
+        ]
+        return custom_urls + urls
+    
+
+@admin.register(OutputDataTabel)
+class OutputDataTabelAdmin(admin.ModelAdmin):
+    list_display = ["__str__"]
+    formfield_overrides = {
+        JSONField: {'widget': JSONEditorWidget},
+    }
